d09284c1784f3d3d94edac69d71e279ec51afcc1
adilkhan <adilkhan@ucsb.edu>
2020-04-14 11:15:29 -0700

    Testing

---
+++ slidingWindowPattern.js
@@ -0,0 +1,8 @@
+// Sliding window Pattern :
+// --useful when we are given an array or any object that has sequential element list and we want to find a contigous sublist in that list.
+
+// Example Problem #1 : 
+// Given an array, find the average of all contiguous subarrays of size ‘K’ in it.
+//  Inputs : Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
+
+
