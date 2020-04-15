f37df8c5373a44a786d58661a5e2ebdb670e70b8
adilkhan <adilkhan@ucsb.edu>
2020-04-15 15:01:31 -0700

    first example for sliding window pattern is done

--- slidingWindowPattern.js
+++ slidingWindowPattern.js
@@ -7,22 +7,52 @@
 //  Inputs : Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
 // --------Naive Solution :
 const avgSum = (arr, n) => {
-    let max= -Infinity;
+    // initialize an empty array 
+    const results = [];
+    // check for edge cases.
+    if(n>arr.length){
+        return null
+    }
     for(let i=0; i<arr.length-n+1; i++){
-        // Initialize a temp variable that will start at 0 during each iteration of the for loop.
-        let temp = 0;
-        // Create another for loop that will iterate over the next n numbers and add its total to the temp variable.
+        // Initialize a sum variable that will start at 0 during each iteration of the for loop.
+        // Sum will hold the value through each loop then compare that to the max. 
+        let sum = 0;
+        // Create another for loop that will iterate over the next n numbers and add its total to the sum variable.
         for(let j=i; j<i+n; j++){
-            temp += arr[i+j];
+            sum += arr[j];
         }
-        // If the temp variable is greater than the max then max = temp.
-        if(temp > max){
-            max = temp
+        results.push(sum/n)
+    }
+    console.log("results 1: ", results)
+}
+// Refactored solution : 
+/*
+*/
+const avgSum2 = (arr, k) =>{
+    let results = [];
+    /*
+    initialize a pointer that will act as the back of the window and moving up after each a for loop.
+    */
+    let windowSum = 0.0;
+    let windowStart = 0
+    for(let i=0; i< arr.length; i++){
+        // Add the first k elements to the windowSum variable
+        windowSum += arr[i];
+        if(i >= k - 1){
+            // Push the result of the first window.
+            results.push(windowSum/k);
+            // subtract the first element from the window after adding it.
+            windowSum -= arr[windowStart];
+            // Move up the pointer
+            windowStart++
         }
     }
-    console.log(max)
+    console.log("results 2: ", results)
 }
+avgSum([1, 3, 2, 6, -1, 4, 1, 8, 2], 5);
+avgSum2([1, 3, 2, 6, -1, 4, 1, 8, 2], 5);
 
-avgSum([1, 3, 2, 6, -1, 4, 1, 8, 2], 5)
+// Example problem #2 : 
+// 
 
 
